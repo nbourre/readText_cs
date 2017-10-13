9b960f7a23d1902d8bfa46201e6dc302748a793a
nbourre <nbourre@hotmail.com>
2017-10-13 15:02:31 -0400

    Premier jet

--- readText_cs/Program.cs
+++ readText_cs/Program.cs
@@ -3,13 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace readText_cs
 {
     class Program
     {
+        static string filename;
+        static string input;
+        static StreamReader file;
+
+        static int nbTest;
+
         static void Main(string[] args)
         {
+            filename = args[0];
+
+            if (filename == string.Empty)
+            {
+                Console.WriteLine("Aucun fichier fourni" + Environment.NewLine);
+                Environment.Exit(-1);
+            }
+
+            file = File.OpenText(filename);
+
+            input = file.ReadToEnd();
+
+            nbTest = int.Parse(file.ReadLine());
+
+            Console.Write(input);
+            Console.ReadKey();
+
         }
     }
 }
